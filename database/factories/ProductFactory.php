f567801bdc65c9fdea42c45c4be439123e54099c
AdipHabibullah <riyiknjir@gmail.com>
2026-04-21 13:09:34 +0700

    project tugas

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::inRandomOrder()->first()->id, // ✅ ambil kategori acak
+            'name'        => fake()->word(),
+            'price'       => fake()->numberBetween(10000, 100000),
+            'stock'       => fake()->numberBetween(1, 100),
+        ];
+    }
+}
